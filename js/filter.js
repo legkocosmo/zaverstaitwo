ad822d3698ce36d9e13fd0bae4440fafd0b703de
legkocosmo <mylegko@gmail.com>
2020-10-15 11:58:26 +0300

    new js

--- js/filter.js
+++ js/filter.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let articles = document.querySelectorAll('.card');
 let filter = document.querySelector('.filter');
 
@@ -10,17 +9,4 @@ filter.onchange = function () {
       article.classList.remove('hidden');
     }
   }
-=======
-let articles = document.querySelectorAll('.card');
-let filter = document.querySelector('.filter');
-
-filter.onchange = function () {
-  for (let article of articles) {
-    if (article.dataset.category !== filter.value && filter.value !== 'all') {
-      article.classList.add('hidden');
-    } else {
-      article.classList.remove('hidden');
-    }
-  }
->>>>>>> ddd8bf7ffd522bad23d69614e458f34e3663d7da
 };
--- js/like.js
+++ js/like.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let like = document.querySelector('.like');
 let likesNumber = document.querySelector('.likes-number');
 
@@ -10,16 +9,3 @@ like.onclick = function () {
   }
   like.classList.toggle('added');
 }; 
-=======
-let like = document.querySelector('.like');
-let likesNumber = document.querySelector('.likes-number');
-
-like.onclick = function () {
-  if (like.classList.contains('added')) {
-    likesNumber.textContent--;
-  } else {
-    likesNumber.textContent++;
-  }
-  like.classList.toggle('added');
-}; 
->>>>>>> ddd8bf7ffd522bad23d69614e458f34e3663d7da
